4a571cf3821dee848ea13d66de3e698ea0241f17
Jérémy Touzy <jtouzy2@gmail.com>
2022-06-24 09:50:14 +0200

    [init] Project structure

---
+++ TemplateSelector/ContentView.swift
@@ -0,0 +1,14 @@
+//
+//  ContentView.swift
+//  TemplateSelector
+//
+//  Created by Jérémy TOUZY on 24/06/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  var body: some View {
+    Text("TemplateSelector")
+  }
+}
---
+++ TemplateSelector/TemplateSelectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TemplateSelectorApp.swift
+//  TemplateSelector
+//
+//  Created by Jérémy TOUZY on 24/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct TemplateSelectorApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
